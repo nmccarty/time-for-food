84d63a9962c28d9d73e904e92a7547fa1aec4f66
Nathan McCarty <nathan@mccarty.io>
2018-04-25 19:11:55 -0400

    Add fields to recipe

--- src/food/mod.rs
+++ src/food/mod.rs
@@ -1,4 +1,5 @@
 /// This module contains dumb data structures describing real-world foods
+use num_rational::*;
 
 /// Describes a specific, real world food
 ///
@@ -48,5 +49,24 @@ pub struct RawFood {
 /// A recipe knows its name, its components foods, the ammounts required, the steps
 /// required to produce the recipe, the nutritonal value of the resulting food,
 /// how many servings it produces, and how long the recipe takes to make.
-pub struct Recipe;
-
+pub struct Recipe {
+    /// The name of the Recipe as an encoded String
+    ///
+    /// The name wrapper type is used to help with i18n
+    name: Name,
+    /// Contains the serving size and unit, encoded as an Amount
+    serving_size: Amount,
+    /// Number of servings the recipe makes.
+    ///
+    /// This is stored as a rational to prevent conflicts between the assumed
+    /// reality and actual reality. While it would be nice to have this as in integer
+    /// And force recipes to make a whole number of servings, some recipies, as input,
+    /// may make a fractional number of servings.
+    servings: Rational32,
+    /// Contains the component foods and ammounts there of
+    foods: Vec<(Food,Amount)>,
+    /// Contains the steps, in order, required to produce the recipe
+    steps: Vec<Step>,
+    /// How long the recipe takes to make, in miniutes
+    time: Rational32,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+extern crate num_rational;
+
 pub mod food;
 fn main() {
     println!("Hello, world!");
