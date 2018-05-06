b75e5d313a6122e940c6178fa45c5ee90d958af4
Nathan McCarty <nathan@mccarty.io>
2018-05-05 22:34:01 -0400

    Remove rocket depency, switch back to stable
    Unstable rust is too much of a pain in the ass

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 #![feature(plugin, decl_macro)]
 #![plugin(rocket_codegen)]
 extern crate num_rational;
-#[macro_use] extern crate rocket;
 extern crate serde;
 extern crate serde_json;
 #[macro_use] extern crate serde_derive;
